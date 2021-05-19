ad3f4a6ec36fae302d070f796f3e996c373838de
ЭДУАРД <edvard984@mail.ru>
2021-05-19 13:45:31 +0500

    Initial commit

---
+++ src/main/java/com/game/service/PlayerService.java
@@ -0,0 +1,41 @@
+package com.game.service;
+
+import com.game.entity.Player;
+import com.game.entity.Profession;
+import com.game.entity.Race;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public interface PlayerService {
+
+    List<Player> getAllPlayersList(Specification<Player> specification);
+
+    Page<Player> getAllPlayersList(Specification<Player> specification, Pageable sortedByName);
+
+    Player createPlayer(Player requestPlayer);
+
+    Player getPlayer(Long id);
+
+    Player updatePlayer(Long id, Player requestPlayer);
+
+    boolean deletePlayer(Long id);
+
+    Specification<Player> nameFilter(String name);
+
+    Specification<Player> titleFilter(String title);
+
+    Specification<Player> raceFilter(Race race);
+
+    Specification<Player> professionFilter(Profession profession);
+
+    Specification<Player> experienceFilter(Integer min, Integer max);
+
+    Specification<Player> levelFilter(Integer min, Integer max);
+
+    Specification<Player> birthdayFilter(Long after, Long before);
+
+    Specification<Player> bannedFilter(Boolean isBanned);
+}
